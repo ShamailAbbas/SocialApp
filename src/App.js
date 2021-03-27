e7cf2c4beda85b359038a306a4bda1b12a41e1ff
Shamail <shamailabbas61@gmail.com>
2021-03-28 04:47:40 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { React } from "react";
-import Feeds from "./components/Feeds.js";
 import Navbar from "./components/Navbar.js";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Upload from "./components/Upload.js";
+
 import Landingpage from "./components/Landingpage.js";
 import "./css/App.css";
 import Timeline from "./components/Timeline.js";
